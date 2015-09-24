cb168300c526aab03fb07fc60cc20454c3b45029
Simon Templer <simon@wetransform.to>
2015-09-24 11:29:32 +0200

    encode using featureMember

--- scripts/setup.groovy
+++ scripts/setup.groovy
@@ -10,11 +10,23 @@ def updateWfsConfiguration() {
   def parser = new XmlParser(false, true)
   def wfsConfig = parser.parseText(file.text)
 
+  /*
+   * Following the recommended settings as documented on
+   * http://docs.geoserver.org/latest/en/user/data/app-schema/wfs-service-settings.html
+   */
+
   // make sure canoncial schema location is enabled
+  // <canonicalSchemaLocation>true</canonicalSchemaLocation>
   def canonicalSchemaLocation = wfsConfig.canonicalSchemaLocation[0] ?: parser.createNode(
     wfsConfig, new QName('canonicalSchemaLocation'), [:]
   )
   canonicalSchemaLocation.value = true
+
+  // <encodeFeatureMember>true</encodeFeatureMember>
+  def encodeFeatureMember = wfsConfig.encodeFeatureMember[0] ?: parser.createNode(
+    wfsConfig, new QName('encodeFeatureMember'), [:]
+  )
+  encodeFeatureMember.value = true
 
   file.text = XmlUtil.serialize(wfsConfig)
 }

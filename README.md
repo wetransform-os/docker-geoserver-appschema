2db87263bc39fdb38f355a2685084d523994f47e
Simon Templer <simon@wetransform.to>
2015-09-25 10:10:06 +0200

    improved setting passwords
    If an admin password is set, the master password should be set too.
    Store the password digest if a plain password is provided.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # docker-geoserver-appschema
 Simple Dockerfile to build an image running GeoServer (plus app-schema extension) on Tomcat 6 and JRE 7.
+
+## Configuration from host
+
+Example of mounting a GeoServer data directory present on the Docker host:
+
+```
+docker run -it -p 8080:8080 -v /data/dir/on/host:/opt/webapps/geoserver/data wetransform/geoserver-appschema
+```
+
+## Environment variables
+
+`GS_ADMIN_PASSWORD`
+`GS_MASTER_PASSWORD`
+
+These variables can be used to conveniently set administrator and master passwords to replace the default ones if you do not use a custom data directory but don't want to use the default passwords (e.g. in publicly accessible test environments). Passwords are expected to provided as
+
+* GeoServer digest encrypted password (`digest1:hashedPasswordAndSalt`) **recommended**
+* GeoServer plain text password (`plain:password`)
+* or simply the plain password
+
+If an admin password is provided but no master password, the admin password is used as the master password as well.
+
+Example call:
+
+```
+docker run -it -p 8080:8080 -e GS_ADMIN_PASSWORD=mysecretpassword wetransform/geoserver-appschema
+```
--- scripts/prelaunch.groovy
+++ scripts/prelaunch.groovy
@@ -1,38 +1,66 @@
 #!/usr/bin/env groovy
 import groovy.xml.*
 
+@Grab(group='org.jasypt', module='jasypt', version='1.9.2')
+import org.jasypt.util.password.StrongPasswordEncryptor
+
+/*
+ * Encodes passwords similar to GeoServer (using StrongPasswordEncryptor).
+ *
+ * see https://github.com/geoserver/geoserver/blob/master/src/main/src/main/java/org/geoserver/security/password/GeoServerDigestPasswordEncoder.java
+ */
+class GeoServerDigest {
+
+  final StrongPasswordEncryptor encryptor
+
+  GeoServerDigest() {
+    encryptor = new StrongPasswordEncryptor();
+  }
+
+  String convertPassword(String passwd) {
+    if (!passwd) {
+      // no password provided
+      null
+    }
+    else if (passwd.startsWith('digest1:')) {
+      // use as provided
+      passwd
+    }
+    else if (passwd.startsWith('plain:')) {
+      // create digest password
+      passwd = passwd[6..-1]
+      'digest1:' + encryptor.encryptPassword(passwd)
+    }
+    else {
+      // assume plain password
+      'digest1:' + encryptor.encryptPassword(passwd)
+    }
+  }
+
+}
+
 // definitions
 basePath = new File(args[0])
 
 def setDefaultPasswords() {
-  // Password is expected to be in the pattern expected by GeoServer
-  // e.g. "digest1:base64encodedPasswdAndSalt"
-  def masterPasswd = System.env.GS_MASTER_PASSWORD
-  def masterFile = new File(basePath, 'data/security/masterpw.digest')
-  if (masterPasswd) {
-    if (masterPasswd.startsWith('digest1:')) {
-      masterFile.text = masterPasswd
-      println "Master password set"
-    }
-    else {
-      println 'Unrecognized/unsupported master password pattern'
-    }
+  def passwords = new GeoServerDigest()
+
+  // Passwords are expected to be in the pattern expected by GeoServer
+  // with either "digest1:..." or "plain:..." or as a plain password
+  def masterPasswd = passwords.convertPassword(System.env.GS_MASTER_PASSWORD)
+  def adminPasswd = passwords.convertPassword(System.env.GS_ADMIN_PASSWORD)
+  if (!masterPasswd) {
+    masterPasswd = adminPasswd
   }
 
-  // Password is expected to be in the pattern expected by GeoServer
-  // with either "digest1:..." or "plain:..." or as a plain password
-  def adminPasswd = System.env.GS_ADMIN_PASSWORD
+  /*
+   * Set admin password
+   */
+
   def usersFile = new File(basePath, 'data/security/usergroup/default/users.xml')
   if (adminPasswd) {
-    if (!adminPasswd.startsWith('plain:') || !adminPasswd.startsWith('digest1:')) {
-      // assuming plain password
-      adminPasswd = "plain:$adminPasswd"
-      println 'Setting plain admin password'
-    }
-    else {
-      println 'Setting configured pattern as admin password'
-    }
-
+    println 'Setting admin password'
+    
     def parser = new XmlParser(false, true)
     def userRegistry = parser.parseText(usersFile.text)
 
@@ -52,6 +80,23 @@ def setDefaultPasswords() {
     }
 
     usersFile.text = XmlUtil.serialize(userRegistry)
+  }
+
+  /*
+   * Set master password
+   */
+  
+  //XXX what about keystore? needed to recreated / change password?
+
+  def masterFileDigest = new File(basePath, 'data/security/masterpw.digest')
+  if (masterPasswd) {
+    if (masterPasswd.startsWith('digest1:')) {
+      masterFileDigest.text = masterPasswd
+      println "Master password set (digest1)"
+    }
+    else {
+      throw new IllegalStateException('Unrecognized/unsupported master password pattern')
+    }
   }
 }
 
